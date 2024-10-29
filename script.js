8b9fd81c743ea3e37725c25c18f92158528d9266
PRIYANVI GUPTA <priyanvigupta869@gmail.com>
2024-10-29 23:15:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,3 @@ const navbar = document.querySelector(".navbar");
 menuIcon.addEventListener("click", () => {
     navbar.classList.toggle("change")
 })
-document.getElementById("priya").onclick=function(){
-    alert("Your email has been submitted imaginary because this is only a trial project,Thank you!!.");
-}
